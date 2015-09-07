606af29fb67f1b1ef69cacb652881793679744ff
Max Verigin <leetkr3w@gmail.com>
2015-09-07 11:09:04 -0700

    add blacklist and whitelist

---
+++ services/whitelist.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+
+var file = '../data/specialCommandWhitelistUsers.json';
+
+var whitelist = {};
+
+loadWhitelist();
+
+function saveWhitelist() {
+    fs.writeFileSync(file, JSON.stringify(whitelist));
+    return;
+}
+
+function loadWhitelist() {
+    try {
+        if (fs.existsSync(file)) {
+            whitelist = JSON.parse(fs.readFileSync(file, 'utf8')) || {};
+        } else {
+            saveWhitelist();
+        }
+    } catch (e) {
+        console.log(e);
+    }
+    return whitelist;
+}
+
+module.exports = {
+    isWhitelisted: function (username) {
+        return !!whitelist[username];
+    },
+    add: function (username) {
+        if (username !== undefined) {
+            whitelist[username] = true;
+            saveWhitelist();
+        }
+        return !!whitelist[username];
+    },
+    remove: function (username) {
+        if (username !== undefined) {
+            whitelist[username] = false;
+            saveWhitelist();
+        }
+        return !!whitelist[username];
+    }
+}
